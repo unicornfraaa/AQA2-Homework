217ea0c3a4f78b4a423150c985d5768e53e3409c
Ksenia <ksuskaxD@gmail.com>
2022-08-25 11:34:51 +0300

    added tests v1

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -18,4 +18,19 @@ class MobileBankApiTestV1 {
                 .then()
                 .statusCode(200);
     }
+
+    @Test
+    void shouldCheckConnectionHeader() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .header("connection", "keep-alive")
+        ;
+    }
 }
