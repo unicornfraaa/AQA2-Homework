39b013a500ac59b085f82931629f0022090d0c80
Ksenia <ksuskaxD@gmail.com>
2022-08-25 11:52:25 +0300

    added tests v5 v6

--- src/test/java/ru/netology/rest/MobileBankApiTestV5.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV5.java
@@ -11,18 +11,34 @@ import static org.hamcrest.Matchers.*;
 class MobileBankApiTestV5 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          .contentType(ContentType.JSON)
-          .body("every{ it.balance >= 0 }", is(true))
-      ;
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("every{ it.balance >= 0 }", is(true))
+        ;
+    }
+
+    @Test
+    void shouldReturnCurrentElementValues() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("id[0].equals(1)", is(true))
+                .body("balance[2].equals(1204044)", is(true));
+
     }
 }
--- src/test/java/ru/netology/rest/MobileBankApiTestV6.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV6.java
@@ -6,29 +6,48 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.core.IsEqual.equalTo;
+
 import static io.restassured.RestAssured.given;
 
 class MobileBankApiTestV6 {
     private RequestSpecification requestSpec = new RequestSpecBuilder()
-        .setBaseUri("http://localhost")
-        .setBasePath("/api/v1")
-        .setPort(9999)
-        .setAccept(ContentType.JSON)
-        .setContentType(ContentType.JSON)
-        .log(LogDetail.ALL)
-        .build();
+            .setBaseUri("http://localhost")
+            .setBasePath("/api/v1")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
 
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .spec(requestSpec) // со спецификацией проще (особенно когда много тестов)
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+        // Given - When - Then
+        // Предусловия
+        given()
+                .spec(requestSpec) // со спецификацией проще (особенно когда много тестов)
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void shouldCheck() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .spec(requestSpec) // со спецификацией проще (особенно когда много тестов)
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("name[0]", equalTo("Текущий счёт"),
+                        "name[1]", equalTo("Текущий счёт"),
+                        "name[2]", equalTo("Текущий зарплатный счёт"));
     }
 }
